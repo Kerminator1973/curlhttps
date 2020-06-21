f4e3a6cf7aa75176b600e7cd3e70650a6d0a0696
Maksim Rozhkov <kermit@mail.ru>
2020-06-21 17:41:54 +0300

    Скрипт выкачивает исходники библиотек zlib, openssl и curl

--- build_lib.bat
+++ build_lib.bat
@@ -1,49 +1,81 @@
+echo off
+
+REM goto BUILD
+
+REM Предусловие - инструментарий git должен быть установлен: https://git-scm.com/downloads
+REM
+REM Первый параметр (%1) - целевой подкаталог, в который следует загрузить 
+REM репозитарии исходных текстов: Curl, zlib и openSSL
+
+REM Проверяем наличие параметров
+IF "%~1" == "" GOTO EMPTY_ARGS
+
+REM Проверяем, существует ли указанный подкаталог
+IF EXIST %1 GOTO CLONE
+
+REM Создаём указанный подкаталог (рекурсивно)
+mkdir "%1"
+
+:CLONE
+pushd .
+cd "%1"
+echo Downloading the source files of the GitHub
+git clone https://github.com/curl/curl.git
+git clone https://github.com/madler/zlib
+git clone https://github.com/openssl/openssl 
+popd
+
+REM Следующий этап - сборка исходных библиотеки из исходных текстов
+:BUILD
+echo TODO: compile everything
+
+
+echo Finished successfully
+
+GOTO EXIST
+
+:EMPTY_ARGS
+
+echo Please, specify the directory to store all required repositories.
+echo Example:
+echo 	build_lib.bat d:/Sources/
+
+GOTO EXIST
+
 REM КЛЮЧЕВЫМ РЕЗУЛЬТАТОМ СБОРКИ ЯВЛЯЕТСЯ БИБЛИОТЕКА:
 REM c:\curl\builds\libcurl-vc12-x86-debug-static-zlib-static-obj-lib\libcurl_a_debug.lib 
 
-echo off
-
-REM Предусловие - инструментарий git должен быть установлен: https://git-scm.com/downloads
-echo Downloading the source files of the Curllib from the GitHub
-
-pushd .
-
-REM Мы созадём папку в корневом подкаталоге
-cd "c:\"
-
-git clone https://github.com/curl/curl.git
-REM Результат - должны быть загружены исходные тексты библиотеки в папку "curl"
-REM После сборки исходников, папку можно будет удалить для экономии места на диске
-
-cd curl
+REM cd curl
 
 REM Запускаем конфигурационный скрипт
-echo Running the buildconf.bat
-call c:\curl\buildconf.bat
+REM echo Running the buildconf.bat
+REM call c:\curl\buildconf.bat
 
 REM Копируем собранные в ПроАТМ артефакты в отдельную папку на диске C:/
-echo Copying the existing files of the Zlib from the ProATM's subfolders
-if not exist "depends_debug" mkdir depends_debug
-cd depends_debug
-if not exist "include" mkdir include
-if not exist "lib" mkdir lib
-if not exist "bin" mkdir bin
-popd
+REM echo Copying the existing files of the Zlib from the ProATM's subfolders
+REM if not exist "depends_debug" mkdir depends_debug
+REM cd depends_debug
+REM if not exist "include" mkdir include
+REM if not exist "lib" mkdir lib
+REM if not exist "bin" mkdir bin
+REM popd
 
-copy /Y ..\Common\zlib\*.h c:\curl\depends_debug\include\*
-copy /Y Debug\zlib.lib c:\curl\depends_debug\lib\*
+REM copy /Y ..\Common\zlib\*.h c:\curl\depends_debug\include\*
+REM copy /Y Debug\zlib.lib c:\curl\depends_debug\lib\*
 
-pushd .
+REM pushd .
 
-cd c:\curl\winbuild
+REM cd c:\curl\winbuild
 
 rem echo Setting up the environment (Microsoft Visual Studio 2013 Tools)
-call "%VS120COMNTOOLS%\vsvars32.bat"
+REM call "%VS120COMNTOOLS%\vsvars32.bat"
 
 REM Пока не активирован SSL: WITH_SSL=static SSL_PATH=..\depends_debug
 rem echo Building the CUrl (Debug mode)
-call nmake /f Makefile.vc mode=static DEBUG=yes VC=12 WITH_ZLIB=static ZLIB_PATH="..\depends_debug" ENABLE_SSPI=no ENABLE_IPV6=no ENABLE_IDN=no ENABLE_WINSSL=no
+REM call nmake /f Makefile.vc mode=static DEBUG=yes VC=12 WITH_ZLIB=static ZLIB_PATH="..\depends_debug" ENABLE_SSPI=no ENABLE_IPV6=no ENABLE_IDN=no ENABLE_WINSSL=no
 
-popd
+REM popd
 
-echo Successfully Finished
+REM echo Successfully Finished
+
+:EXIST
